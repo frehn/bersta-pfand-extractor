ba0c5a1007016f51dfc8d627c0ed1d1de2a8663a
Daniel Weller <fraini@gmail.com>
2023-12-24 11:29:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# BerSta Pfand Extractor
+
+# Run
+
+`python ui.py`
+
+# Build Windows Executable
+
+`pyinstaller --onefile ui.py`
---
+++ ui.py
@@ -0,0 +1,44 @@
+from bersta import extract_pfand_from_bersta_rechnung
+
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+
+def browse_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        file_path_label.config(text=file_path)
+        extract_button.config(state="normal")
+
+def extract_pfand():
+    file_path = file_path_label.cget("text")
+    try:
+        result = extract_pfand_from_bersta_rechnung(file_path)
+        result_text.delete(1.0, tk.END)
+        result_text.insert(tk.END, result)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+# Set up the root GUI window
+root = tk.Tk()
+root.title("BerSta Pfand Extractor")
+
+# Create a frame for the file path and browse button
+file_frame = tk.Frame(root)
+file_frame.pack(padx=10, pady=10)
+
+file_path_label = tk.Label(file_frame, text="No file selected", anchor="w")
+file_path_label.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+browse_button = tk.Button(file_frame, text="Browse", command=browse_file)
+browse_button.pack(side=tk.RIGHT)
+
+# Create a frame for the Extract button
+extract_button = tk.Button(root, text="Extract Pfand", state="disabled", command=extract_pfand)
+extract_button.pack(pady=5)
+
+# Create a text widget to display the result
+result_text = tk.Text(root, height=10, width=50)
+result_text.pack(padx=10, pady=10)
+
+root.mainloop()
